525e91ddb906c7fdf9cf971a973abec058d39c83
Rafael Lagemann <rdlagemann@gmail.com>
2018-02-02 02:06:14 -0200

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+(function(){
+  const config = {
+    canvas: document.getElementById('c'),
+    image: 'assets/stormtrooper.jpg',
+    fitCanvas: true,
+    uniforms: {
+      pixel_h: 12,
+      pixel_w: 12
+    }
+  };
+
+  // load default
+  let stormtroopers = new Image();
+  stormtroopers.crossOrigin = 'Access-Control-Allow-Origin';
+  stormtroopers.src = 'stormtroopers.jpg';
+  
+  stormtroopers.onload = function() {
+    config.image = stormtroopers;
+    shaderfy.render('pixelation', config);
+  }
+
+
+
+})()
